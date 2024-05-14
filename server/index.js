cc64453ec108dde9a6ab0c48b28cbb8ca587f227
Vishnu Gopalakrishnan <74191166+VishnuGPillai@users.noreply.github.com>
2024-05-14 14:04:57 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 const app = express();
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
+app.use(cors({ origin: 'https://vishnu-mern-blog.vercel.app/', credentials: true }))
 
 
 app.use(upload())
